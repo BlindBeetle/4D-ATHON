7401603b01b572570a0ab72b02b208b11cab387f
BlindBeetle <poyraz@aksu.gen.tr>
2024-12-08 13:25:54 +0300

    Clean-Ocean-Initiative

--- static/index.js
+++ static/index.js
@@ -4,12 +4,12 @@ function handleResponse(data) {
   const totalVolunteerElement = document.getElementById("totalDonors");
 
   if (data.status === "success") {
-      totalMoneyElement.textContent = `$${data.values.total_donations.toFixed(2)}`;
-      totalTrashElement.textContent = `${data.values.trash_ratio.toFixed(2)} kg`;
-      totalVolunteerElement.textContent = `${data.values.volunteer_count.toFixed(2)}`;
-  } else {
-      console.error("Error in response:", data.message);
-  }
+    totalMoneyElement.textContent = `$${data.values.total_donations.toLocaleString()}`;
+    totalTrashElement.textContent = `${data.values.trash_ratio.toLocaleString()} kg`;
+    totalVolunteerElement.textContent = `${data.values.volunteer_count.toLocaleString()}`; 
+} else {
+    console.error("Error in response:", data.message);
+}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
